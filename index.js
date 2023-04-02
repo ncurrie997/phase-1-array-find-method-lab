4a336234d1ec726bd4b15d669be8aabe62499531
Nick Currie <gosox997@gmail.com>
2023-04-02 11:13:48 -0400

    all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+const record = [
+    {year: "2015", result: "W"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "L"},
+]
+
+function superbowlWin(array) {
+  const winningYear = array.find(element => element.result === "W");
+
+  if (winningYear === undefined) {
+    
+  return undefined;
+} else {
+    return winningYear.year
+  }
+}
